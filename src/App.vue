f713977e44a62a121b74412ba38fdbee42f031b7
richard <richard.krijnse-locker@capgemini.com>
2020-03-26 21:42:06 +0100

    Responsive fonts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,57 @@
 <template>
     <div id="app">
+        <custom-header></custom-header>
         <router-view/>
+        <custom-footer></custom-footer>
     </div>
 </template>
 
+<script>
+    import CustomHeader from '@/components/layout/CustomHeader.vue';
+    import CustomFooter from '@/components/layout/CustomFooter.vue';
+
+    export default {
+        components: {
+            CustomHeader,
+            CustomFooter,
+        },
+    };
+</script>
+
 <style lang="scss">
-    #app {
-        box-sizing: border-box;
+    *,
+    *::after,
+    *::before {
+        box-sizing: inherit;
         margin: 0;
         padding: 0;
-        font-family: Montserrat,serif;
+        font-family: Montserrat, serif;
+    }
+
+    html {
+        font-size: 62.5%;
+
+        @media only screen and (max-width: 56.25em) { //900 px
+            font-size: 50%;
+        }
+        @media only screen and (max-width: 75em) { //1200px
+            font-size: 56.25%;
+        }
+        @media only screen and (min-width: 112.5em) { //1800px
+            font-size: 75%;
+        }
+    }
+
+    body {
+        font-family: Montserrat, serif;
+        box-sizing: border-box;
+    }
+
+    .text-white {
+        color: #fff;
+    }
+
+    .text-black {
+        color: #071E22;
     }
 </style>
