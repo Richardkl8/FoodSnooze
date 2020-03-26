04603e2bc475ae26b997d956a8fa37e5186876d0
richard <richard.krijnse-locker@capgemini.com>
2020-03-26 21:41:16 +0100

    Global components

---
+++ src/global/globalComponents.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import CustomH1 from '@/components/typography/CustomH1.vue';
+import CustomH2 from '@/components/typography/CustomH2.vue';
+import CustomH3 from '@/components/typography/CustomH3.vue';
+
+Vue.component('custom-h1', CustomH1);
+Vue.component('custom-h2', CustomH2);
+Vue.component('custom-h3', CustomH3);
