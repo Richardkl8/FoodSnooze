6ddec9e5605df6f5bbf033da7b0ab6df95122a37
richard <richard.krijnse-locker@capgemini.com>
2020-03-28 10:10:33 +0100

    Router no cache

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Recipe from '../views/Recipe.vue';
+import Search from '../views/Search.vue';
 
 Vue.use(VueRouter);
 
@@ -13,18 +15,12 @@ const routes = [
     {
         path: '/recipe/:id',
         name: 'Recipe',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Recipe.vue'),
+        component: Recipe,
     },
     {
         path: '/search/:query',
         name: 'Search',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue'),
+        component: Search,
     },
 ];
 
