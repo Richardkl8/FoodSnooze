45490332795165c725cd32c1c641662e1c3c00df
richard <richard.krijnse-locker@capgemini.com>
2020-03-28 10:11:27 +0100

    Custom button style

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,7 +1,10 @@
 <template>
-    <button>
-        {{text}}
-    </button>
+    <div class="button">
+        <div class="circle"></div>
+        <a>
+            {{text}}.
+        </a>
+    </div>
 </template>
 
 <script>
@@ -14,3 +17,59 @@
         },
     };
 </script>
+
+
+<style lang="scss" scoped>
+    .button {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+        position: relative;
+        padding: 1rem 3rem;
+        background-color: #2DB396;
+        color: #fff;
+        font-size: 1.8em;
+        font-weight: bold;
+        text-transform: capitalize;
+        text-decoration: none;
+        transition: all .2s ease-Out;
+
+        &:hover {
+            color: #071E22;
+            cursor: pointer;
+            transform: translateY(-2px);
+            box-shadow: rgba(0, 0, 0, 0.13) 3px 3px 3px ;
+        }
+
+        &:active {
+            transform: translateY(0px);
+            box-shadow: none;
+        }
+
+        &:hover .circle {
+            width: 200%;
+            height: 500%;
+            opacity: 1;
+            top: -7rem;
+            left: -7rem;
+        }
+
+        a {
+            z-index: 1;
+        }
+    }
+
+    .circle {
+        width: 0;
+        height: 0;
+        opacity: 0;
+        border-radius: 50%;
+        background: #FF6F59;
+        position: absolute;
+        transition: all .4s ease-Out;
+        top: 2rem;
+        left: 7rem;
+    }
+
+</style>
