1340700e16c99710ca6f6a19b07186b547c9c933
richard <richard.krijnse-locker@capgemini.com>
2020-03-28 10:16:46 +0100

    Recipe card responsive

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -4,17 +4,14 @@
              class="recipe-image"
              :src="recipe.strMealThumb">
         <div class="recipe-text center">
-            <custom-h2>
+            <custom-h3>
                 <b>{{recipe.strMeal}}</b>
-            </custom-h2>
+            </custom-h3>
             <custom-h3 v-if="isRelatedFoods">
                 {{recipe.strCategory}}
             </custom-h3>
-            <custom-h3 v-if="isRelatedFoods">
-                {{recipe.strTags}}
-            </custom-h3>
         </div>
-        <div v-if="isRelatedFoods" class="separator"></div>
+<!--        <div v-if="isRelatedFoods" class="separator"></div>-->
     </div>
 </template>
 
@@ -50,6 +47,8 @@
         overflow: hidden;
         max-width: 50rem;
         cursor: pointer;
+        display: flex;
+        flex-direction: column;
 
         &:hover .recipe-image {
             transform: scale(1.02);
@@ -73,5 +72,22 @@
 
     .recipe-text {
         margin: 2rem 0;
+    }
+
+    @media screen and (max-width: 768px) {
+        .recipe-card {
+            flex-direction: row;
+            margin-bottom: 2rem;
+        }
+
+        .recipe-image {
+            width: 50%;
+            height: 50%;
+            margin-right: 2rem;
+        }
+
+        .recipe-text {
+            margin: 0 0;
+        }
     }
 </style>
